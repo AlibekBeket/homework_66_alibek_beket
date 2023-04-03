74a9d2885b1146726735fa94af320c080b965b97
Alibek Beket <alibek1beket@gmail.com>
2023-04-03 22:37:06 +0600

    сделал второе задание

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-console.log('task 1')
-let lineOrder = [3, 5, 2, 6, 4, 1]
+console.log('task 1');
+let lineOrder = [3, 5, 2, 6, 4, 1];
 for(let i = 0; i < lineOrder.length; i++) {
     console.log(document.getElementById(`string-${lineOrder[i]}`).childNodes[0].textContent);
+};
+//task 2
+const elements = document.querySelectorAll('.element');
+for(let i = 0; i < elements.length; i++) {
+    if (i < 3){
+        elements[i].style.color = 'red';
+    }
+    else if (i < 6) {
+        elements[i].style.color = 'green';
+    };
 };
